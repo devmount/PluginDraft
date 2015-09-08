f263a629e5903f71cc9e674db312ff387a0b8241
Andreas M <mail@devmount.de>
2015-09-09 00:43:46 +0200

    fixed: broken github link

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
  * @author   DEVMOUNT <mail@devmount.de>
  * @license  GPL v3+
  * @version  GIT: v0.x.jjjj-mm-dd
- * @link     https://github.com/devmount/PluginDraft
+ * @link     https://github.com/devmount-mozilo/PluginDraft
  * @link     http://devmount.de/Develop/moziloCMS/Plugins/PluginDraft.html
  * @see      Verse
  *           – The Bible
@@ -34,7 +34,7 @@ if (!defined('IS_CMS')) {
  * @package  PHP_MoziloPlugins
  * @author   DEVMOUNT <mail@devmount.de>
  * @license  GPL v3+
- * @link     https://github.com/devmount/PluginDraft
+ * @link     https://github.com/devmount-mozilo/PluginDraft
  */
 class PluginDraft extends Plugin
 {
